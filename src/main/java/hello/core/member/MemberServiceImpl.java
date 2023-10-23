3ffbdb35cf81fe11ee3b2efcdce51eb5d0a61c06
Kim ByungWoo <kimjoshua135@gmail.com>
2023-10-23 16:32:59 +0900

    java 2 spring

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,7 +1,12 @@
 package hello.core.member;
 
 public class MemberServiceImpl implements  MemberService{
-    private final MemberRepository memberRepsoitory = new MemoryMemberRepository();
+    private MemberRepository memberRepsoitory;
+
+    public MemberServiceImpl(MemberRepository memberRepsoitory) {
+        this.memberRepsoitory = memberRepsoitory;
+    }
+
     @Override
     public void join(Member member) {
         memberRepsoitory.save(member);
